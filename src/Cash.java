108811bc3317d63ecb7b58161c33c80e9663ae66
Noor <39026914+Hunzaa@users.noreply.github.com>
2020-11-28 01:49:24 +0000

    Updated Cash.java to process 50 and 20 cash (version 2)

--- src/Cash.java
+++ src/Cash.java
@@ -5,15 +5,26 @@ public class Cash {
         this.amount = amt;
     }
 
-    public int getAmount() {
-        return this.amount;
-    }
-
     public static void dispense(Cash cash) {
-        //amount should be greater then or equal to 10
-        if (cash.getAmount() >= 10) {
+        //amount greater than or equal to 50
+        if (cash.getAmount() >= 50) {
+            int number = cash.getAmount() / 50;
+            System.out.println("- Dispensing " + number + " 50€ note(s)");
+        }
+        //amount greater than or equal to 20
+        else if (cash.getAmount() >= 20) {
+            int number = cash.getAmount() / 20;
+            System.out.println("- Dispensing " + number + " 20€ note(s)");
+        }
+        //amount greater than or equal to 10
+        else if (cash.getAmount() >= 10) {
             int number = cash.getAmount() / 10;
             System.out.println("- Dispensing " + number + " 10€ note(s)");
         }
+
+    }
+
+    public int getAmount() {
+        return this.amount;
     }
 }
