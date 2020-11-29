d75ede51191e65755dca76e844506926acd18a1e
Noor <39026914+Hunzaa@users.noreply.github.com>
2020-11-29 01:42:44 +0000

    Refactored Cash.java and added three handler classes

--- src/Cash.java
+++ src/Cash.java
@@ -5,25 +5,6 @@ public class Cash {
         this.amount = amt;
     }
 
-    public static void dispense(Cash cash) {
-        //amount greater than or equal to 50
-        if (cash.getAmount() >= 50) {
-            int number = cash.getAmount() / 50;
-            System.out.println("- Dispensing " + number + " 50€ note(s)");
-        }
-        //amount greater than or equal to 20
-        else if (cash.getAmount() >= 20) {
-            int number = cash.getAmount() / 20;
-            System.out.println("- Dispensing " + number + " 20€ note(s)");
-        }
-        //amount greater than or equal to 10
-        else if (cash.getAmount() >= 10) {
-            int number = cash.getAmount() / 10;
-            System.out.println("- Dispensing " + number + " 10€ note(s)");
-        }
-
-    }
-
     public int getAmount() {
         return this.amount;
     }
---
+++ src/Euro10Dispenser.java
@@ -0,0 +1,22 @@
+public class Euro10Dispenser implements DispenseChain {
+
+    private DispenseChain nextInChain;
+
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        nextInChain = nextChain;
+    }
+
+    @Override
+    public void dispense(Cash cash) {
+        if (cash.getAmount() >= 10) {
+            int number = cash.getAmount() / 10;
+            int remainder = cash.getAmount() % 10;
+            System.out.println("- Dispensing " + number + " 10€ note(s)");
+            if (remainder != 0)
+                nextInChain.dispense(new Cash(remainder));
+        } else {
+            System.out.println("Amount should be greater than or equal to 10.");
+        }
+    }
+}
