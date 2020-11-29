8b01398e9fc50e624aea2156ff2787a8ca3ad5a0
Noor <39026914+Hunzaa@users.noreply.github.com>
2020-11-29 04:41:39 +0000

    Eliminated duplicate code and comments (working)

--- src/Dispenser.java
+++ src/Dispenser.java
@@ -10,22 +10,14 @@ public class Dispenser {
         c1.setNextChain(c2);
         c2.setNextChain(c3);
 
-        int amount = 0;
+        int amount;
         System.out.println("Enter amount to dispense: ");
         Scanner input = new Scanner(System.in);
         amount = input.nextInt();
 
-        //not multiple of 10
         if ((amount % 10 != 0) && (amount > 10)) {
             System.out.println("Amount should be in multiple of 10s.");
-            return;
-        }
-        //less than 10
-        else if (amount < 10) {
-            System.out.println("Error! Not enough amount.");
-            return;
         } else {
-            //process the amount
             c1.dispense(new Cash(amount));
         }
     }
